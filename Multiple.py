93c1301538bfe9dbdc28d72ebf751b2be38de42c
unknown <gangineedibhanuprakash@gmail.com>
2024-07-24 22:28:16 +0530

    1st program

---
+++ Multiple.py
@@ -0,0 +1,9 @@
+def multiply(*args):
+    result = 1
+    for arg in args:
+        result *= arg
+    return result
+print(multiply(2, 3, 4))  
+print(multiply(1.5, 2, 10))  
+print(multiply(5))  
+print(multiply())  
